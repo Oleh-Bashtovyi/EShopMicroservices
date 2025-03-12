d27cee1b66206f3bc8cd430e3512908267d94715
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-03-13 00:09:29 +0200

    add Catalog.API first feature

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+
+// Command that does not return any response
+public interface ICommand : ICommand<Unit>
+{
+}
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,36 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+// ==================================================================================
+
+// Endpoints were created using Carter library.
+// Currently if we install Carter to BuildingBlocks project,
+// it will not Map any classes that implements ICarterModule interface.
+// That`s because it will scan only BuildingBlocks project for those classes.
+// Unfortunately Marten library currently does not have ability
+// To config assembly scan, as MediatR do in next line.
+// So only solution is to install Carter in this project, and not in BuildingBlocks.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+// For products storage we will use PostgreSQL.
+// PostgreSQL has some NoSql db features:
+// (json column features that allows to store and query data as json document)
+// With Marten library we can utilize them.
+// This setup is ideal for handling complex data structures like product entity,
+// Which includes list of the categories and other varied data types
+// and product characteristics.
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP pipeline
+// ==================================================================================
+
+app.MapCarter();
 
 app.Run();
