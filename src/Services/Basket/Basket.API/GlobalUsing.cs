aad317b44f6e73f9dd22e09cbe570f3047ca7315
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-03-15 02:02:03 +0200

    Add Basket.API and 3 endpoints to it

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,12 @@
+﻿global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Exceptions.Handler;
+global using FluentValidation;
+global using Mapster;
+global using MediatR;
+global using Basket.API.Models;
+global using Carter;
+global using Marten;
+global using Basket.API.Exceptions;
+global using Basket.API.Data;
