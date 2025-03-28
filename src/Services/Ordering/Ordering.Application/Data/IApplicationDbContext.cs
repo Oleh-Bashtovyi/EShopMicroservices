c88867d74ffbf17723a612dfb9fb7df318d2a50a
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-03-28 14:33:18 +0200

    Ordering.Application layer completed

---
+++ src/Services/Ordering/Ordering.Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ordering.Application.Data;
+
+public interface IApplicationDbContext
+{
+    public DbSet<Customer> Customers { get; }
+    public DbSet<Order> Orders { get; }
+    public DbSet<OrderItem> OrderItems { get; }
+    public DbSet<Product> Products { get; }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
